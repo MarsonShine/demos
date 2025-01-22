b252f2ad72ae9e64d67e174ec71be7f406c27ee5
marsonshine <ms27946@163.com>
2025-01-22 16:31:45 +0800

    fix: combine multiple files into single file.

---
+++ AudioDemo/Combination.cs
@@ -0,0 +1,76 @@
+﻿using NAudio.Wave;
+
+namespace AudioDemo
+{
+	internal class Combination
+	{
+		/// <summary>
+		/// 将多个音频文件合并为一个音频文件
+		/// </summary>
+		/// <param name="inputFiles">输入的音频文件</param>
+		/// <param name="outputFile">输出的音频文件</param>
+		public static void MixAudioFiles()
+		{
+			// 扫描当前目录sound文件夹下的所有mp3文件
+			var inputFiles = System.IO.Directory.GetFiles("sound", "*.mp3");
+			string outputFile = "sound/combination/output.wav";
+			// 按文件创建时间排序
+			inputFiles = [.. inputFiles.OrderBy(file => new System.IO.FileInfo(file).Name)];
+
+			WaveFormat targetFormat = new(44100, 16, 2); // 目标格式
+			using var outputStream = new WaveFileWriter(outputFile, targetFormat);
+			foreach (var file in inputFiles)
+			{
+				using var reader = new WaveFileReader(file);
+				if (!reader.WaveFormat.Equals(targetFormat))
+				{
+					// 使用 MediaFoundationResampler 进行格式转换，确保所有文件的采样率一致
+					using var resampler = new MediaFoundationResampler(reader, targetFormat);
+					resampler.ResamplerQuality = 60;
+					WriteToOutput(resampler, outputStream);
+				}
+				else
+				{
+					WriteToOutput(reader, outputStream);
+				}
+			}
+
+			// 下面这个方法可能会导致合并的文件的声音不正常
+			//using var outputStream = new WaveFileWriter(outputFile, new WaveFormat(24000,16,1));
+			//foreach (var file in inputFiles)
+			//{
+			//	using var reader = new WaveFileReader(file);
+			//	if (!AreWaveFormatsEqual(outputStream.WaveFormat, reader.WaveFormat))
+			//	{
+			//		throw new InvalidOperationException("所有的 WAV 文件格式必须一致（采样率、通道数等）。");
+			//	}
+
+			//	byte[] buffer = new byte[1024];
+			//	int bytesRead;
+			//	while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+			//	{
+			//		outputStream.Write(buffer, 0, bytesRead);
+			//	}
+			//}
+		}
+
+		static void WriteToOutput(IWaveProvider source, WaveFileWriter output)
+		{
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				output.Write(buffer, 0, bytesRead);
+			}
+		}
+
+		static bool AreWaveFormatsEqual(WaveFormat format1, WaveFormat format2)
+		{
+			return format1.SampleRate == format2.SampleRate &&
+				   format1.Channels == format2.Channels &&
+				   format1.BitsPerSample == format2.BitsPerSample &&
+				   format1.BlockAlign == format2.BlockAlign &&
+				   format1.AverageBytesPerSecond == format2.AverageBytesPerSecond;
+		}
+	}
+}
